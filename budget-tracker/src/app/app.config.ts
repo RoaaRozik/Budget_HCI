b5a0cca5ac9eefa725a65b586762e1aa4df8e8ed
Roaa <rozikroaa@gmail.com>
2025-12-13 01:30:40 +0200

    app.config update

--- budget-tracker/src/app/app.config.ts
+++ budget-tracker/src/app/app.config.ts
@@ -2,13 +2,10 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideAnimations(),  // Required for Angular Material
-    provideHttpClient(),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
   ]
 };
